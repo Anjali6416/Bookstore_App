76ccb49c81b876bc243b0c2f8c0080df14371dfb
Anjali <anjaliprajapati1772001@gmail.com>
2023-06-23 19:51:58 +0530

    navbar modified

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import { FaBeer } from "react-icons/fa";
 import { GiShoppingCart } from "react-icons/gi";
 import { GiBookshelf } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 const Navbar2 = () => {
   return (
-    <Navbar expand="md" style={{background:'rgb(236,239,236)'}}>
+    <Navbar expand="md" style={{ background: "rgb(236,239,236)" }}>
       <Container className="d-flex justify-space-between">
         <Navbar.Brand>
           <Link to={"/"} style={{ textDecoration: "none", color: "black" }}>
@@ -21,29 +20,44 @@ const Navbar2 = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link style={{ padding: "10px 40px", fontWeight: "400",fontSize:'20px' }}>
-              <Link to={"/"} style={{ textDecoration: "none", color: "black" }}>
-                Home
-              </Link>
+            <Nav.Link
+              style={{
+                padding: "10px 40px",
+                fontWeight: "400",
+                fontSize: "20px",
+              }}
+              as={Link}
+              to={"/"}
+            >
+              Home
             </Nav.Link>
 
-            <Nav.Link style={{ padding: "10px 40px", fontWeight: "400", fontSize:'20px' }}>
-              <Link
-                to={"/about"}
-                style={{ textDecoration: "none", color: "black" }}
-              >
-                About
-              </Link>
+            <Nav.Link
+              style={{
+                padding: "10px 40px",
+                fontWeight: "400",
+                fontSize: "20px",
+              }}
+              as={Link}
+              to={"/book-details/7D6IDwAAQBAJ"}
+            >
+              Book Details
+            </Nav.Link>
+            <Nav.Link
+              style={{
+                padding: "10px 40px",
+                fontWeight: "400",
+                fontSize: "20px",
+              }}
+              as={Link}
+              to={"/cart"}
+            >
+              Cart
             </Nav.Link>
           </Nav>
           <Nav className="ms-auto">
-            <Nav.Link className="px-4">
-              <Link
-                to={"/cart"}
-                style={{ textDecoration: "none", color: "black" }}
-              >
-                <GiShoppingCart style={{ fontSize: "30px" }} />{" "}
-              </Link>
+            <Nav.Link className="px-4" as={Link} to={"/cart"}>
+              <GiShoppingCart style={{ fontSize: "30px" }} />{" "}
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
